412e0c6b5c76a486787e5dfb1a85d71ccef8aaeb
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-02-05 08:32:31 -0300

    fix endpoint paths

--- data/js/api.js
+++ data/js/api.js
@@ -1,69 +1,76 @@
 export const Api = {
-  setColor: (color) =>
-    fetch("/color", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(color),
-    }).then((r) => r.json()),
+    setColor: (color) =>
+        fetch("/color", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(color),
+        }).then((r) => r.json()),
 
-  fetchColor: () =>
-    fetch("/color")
-      .then((r) => r.json())
-      .then((data) => {
-        if (data.rgb && typeof data.rgb.r !== "undefined") {
-          return data.rgb;
-        } else {
-          throw new Error("Formato de dados inválido" + JSON.stringify(data));
+    fetchColor: () =>
+        fetch("/color")
+            .then((r) => r.json())
+            .then((data) => {
+                if (data.rgb && typeof data.rgb.r !== "undefined") {
+                    return data.rgb;
+                } else {
+                    throw new Error("Formato de dados inválido" + JSON.stringify(data));
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar cor:", error);
+                return null;
+            }),
+
+    setBright: (bright) =>
+        fetch("/bright", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(bright),
+        }).then((r) => r.json()),
+
+    fetchBright: () => fetch("/bright").then((r) => r.json()),
+
+    fetchEffects: () => fetch("/effect").then((r) => r.json()),
+
+    setEffect: (effect) =>
+        fetch("/effect", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(effect),
+        }).then((r) => r.json()),
+
+    scanWifi: () => fetch("/scan").then((r) => r.json()),
+
+    saveWifi: async (formData) => {
+        const r = await fetch("/savewifi", {
+            method: "POST",
+            body: formData,
+        });
+        if (!r.ok) {
+            let message = "Erro desconhecido no servidor.";
+            try {
+                const err = await r.json();
+                message = err.status || err.message || message;
+            } catch {
+                message = await r.text();
+            }
+            throw new Error(`${r.status}: ${message}`);
         }
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar cor:", error);
-        return null;
-      }),
+        return r.json();
+    },
 
-  setBright: (bright) =>
-    fetch("/bright", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(bright),
-    }).then((r) => r.json()),
+    fetchWifiStatus: () => fetch("/wifistatus").then((r) => r.json()),
 
-  fetchBright: () => fetch("/bright").then((r) => r.json()),
+    firmwareUpdate: (formData) =>
+        fetch("/update", {
+            method: "POST",
+            body: formData,
+        }).then((r) => r.json()),
 
-  fetchEffects: () => fetch("/effects").then((r) => r.json()),
+    fileSystemUpdate: (formData) =>
+        fetch("/fsupdate", {
+            method: "POST",
+            body: formData,
+        }).then((r) => r.json()),
 
-  setEffect: (effect) =>
-    fetch("/effects", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(effect),
-    }).then((r) => r.json()),
-
-  scanWifi: () => fetch("/scan").then((r) => r.json()),
-
-  saveWifi: async (formData) => {
-    const r = await fetch("/savewifi", {
-      method: "POST",
-      body: formData,
-    });
-    if (!r.ok) {
-      let message = "Erro desconhecido no servidor.";
-      try {
-        const err = await r.json();
-        message = err.status || err.message || message;
-      } catch {
-        message = await r.text();
-      }
-      throw new Error(`${r.status}: ${message}`);
-    }
-    return r.json();
-  },
-
-  fetchWifiStatus: () => fetch("/wifistatus").then((r) => r.json()),
-
-  firmwareUpdate: (formData) =>
-    fetch("/update", {
-      method: "POST",
-      body: formData,
-    }).then((r) => r.json()),
 };
--- data/js/settings.js
+++ data/js/settings.js
@@ -1,117 +1,117 @@
 import {
-  renderWifiNetworks,
-  setupPasswordToggle,
-  showMessage,
-  showModal,
+    renderWifiNetworks,
+    setupPasswordToggle,
+    showMessage,
+    showModal,
 } from "./ui.js";
 
 import { Api } from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  setupPasswordToggle("togglePasswordBtn", "ssidPass");
-  setupWifiForm("wifiForm", "wifi-msg");
-  setupFirmwareUpdateForm("firmwareForm");
-  setupFSUpdateForm("fsForm");
-  document.getElementById("scanWifiBtn").addEventListener("click", () => {
-    renderWifiNetworks();
-  });
+    setupPasswordToggle("togglePasswordBtn", "ssidPass");
+    setupWifiForm("wifiForm", "wifi-msg");
+    setupFirmwareUpdateForm("firmwareForm");
+    setupFSUpdateForm("fsForm");
+    document.getElementById("scanWifiBtn").addEventListener("click", () => {
+        renderWifiNetworks();
+    });
 });
 
 // ========== Forms ==========
 export function setupWifiForm(formId, msgContainerId) {
-  const form = document.getElementById(formId);
-  if (!form) return;
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const ssid = form.querySelector("#ssid").value;
-    const pass = form.querySelector("#ssidPass").value;
-    if (!ssid || !pass) {
-      showModal("wifiSaveFailModal", "SSID e senha são obrigatórios.");
-      return;
-    }
-    const formData = new FormData(form);
-    formData.append("ssid", ssid);
-    formData.append("pass", pass);
-    try {
-      showMessage("Conectando...", msgContainerId);
-      const data = await Api.saveWifi(formData);
-      if (data.status === "error") {
-        showModal("wifiSaveFailModal", data.msg);
-        return;
-      }
-      let tentativas = 0;
-      const maxTentativas = 12;
-      const intervalo = 10000;
-      const poll = setInterval(async () => {
-        try {
-          tentativas++;
-          const statusData = await Api.fetchWifiStatus();
-          if (statusData.status === "success") {
-            console.log("Conectado");
-            showMessage("Conectado! Redirecionando...", msgContainerId);
-            clearInterval(poll);
-            setTimeout(() => (window.location.href = "/"), 2000);
-          } else if (
-            statusData.status === "fail" ||
-            tentativas >= maxTentativas
-          ) {
-            console.log("Erro ao conectar. Maximo de tentativas atingido");
-            showModal(
-              "wifiFailModal",
-              "Falha ao conectar. Verifique SSID/senha.",
-            );
-            clearInterval(poll);
-          }
-          console.log("Tentativa numero: " + tentativas);
-        } catch (err) {
-          if (err instanceof TypeError) {
-            tentativas++;
-            console.warn("TypeError capturado, tentando novamente...");
-            if (tentativas >= maxTentativas) {
-              clearInterval(poll);
-              showModal("wifiFailModal", "");
-            }
+    const form = document.getElementById(formId);
+    if (!form) return;
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const ssid = form.querySelector("#ssid").value;
+        const pass = form.querySelector("#ssidPass").value;
+        if (!ssid || !pass) {
+            showModal("wifiSaveFailModal", "SSID e senha são obrigatórios.");
             return;
-          } else {
-            showModal(
-              "wifiFailModal",
-              "Erro de comunicação com o dispositivo.",
-            );
-            clearInterval(poll);
-          }
         }
-      }, intervalo);
-    } catch (e) {
-      console.log("Erro desconhecido", e.message);
-      showModal("wifiFailModal", "Erro de comunicação");
-    }
-  });
+        const formData = new FormData(form);
+        formData.append("ssid", ssid);
+        formData.append("pass", pass);
+        try {
+            showMessage("Conectando...", msgContainerId);
+            const data = await Api.saveWifi(formData);
+            if (data.status === "error") {
+                showModal("wifiSaveFailModal", data.msg);
+                return;
+            }
+            let tentativas = 0;
+            const maxTentativas = 12;
+            const intervalo = 10000;
+            const poll = setInterval(async () => {
+                try {
+                    tentativas++;
+                    const statusData = await Api.fetchWifiStatus();
+                    if (statusData.status === "success") {
+                        console.log("Conectado");
+                        showMessage("Conectado! Redirecionando...", msgContainerId);
+                        clearInterval(poll);
+                        setTimeout(() => (window.location.href = "/"), 2000);
+                    } else if (
+                        statusData.status === "fail" ||
+                        tentativas >= maxTentativas
+                    ) {
+                        console.log("Erro ao conectar. Maximo de tentativas atingido");
+                        showModal(
+                            "wifiFailModal",
+                            "Falha ao conectar. Verifique SSID/senha.",
+                        );
+                        clearInterval(poll);
+                    }
+                    console.log("Tentativa numero: " + tentativas);
+                } catch (err) {
+                    if (err instanceof TypeError) {
+                        tentativas++;
+                        console.warn("TypeError capturado, tentando novamente...");
+                        if (tentativas >= maxTentativas) {
+                            clearInterval(poll);
+                            showModal("wifiFailModal", "");
+                        }
+                        return;
+                    } else {
+                        showModal(
+                            "wifiFailModal",
+                            "Erro de comunicação com o dispositivo.",
+                        );
+                        clearInterval(poll);
+                    }
+                }
+            }, intervalo);
+        } catch (e) {
+            console.log("Erro desconhecido", e.message);
+            showModal("wifiFailModal", "Erro de comunicação");
+        }
+    });
 }
 
 function setupFirmwareUpdateForm(formId) {
-  const form = document.getElementById(formId);
-  if (!form) return;
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    try {
-      Api.firmwareUpdate(formData);
-    } catch (e) {
-      console.log("Erro ao enviar formulario");
-    }
-  });
+    const form = document.getElementById(formId);
+    if (!form) return;
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+        try {
+            Api.firmwareUpdate(formData);
+        } catch (e) {
+            console.log("Erro ao enviar formulario");
+        }
+    });
 }
 
 function setupFSUpdateForm(formId) {
-  const form = document.getElementById(formId);
-  if (!form) return;
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    try {
-      Api.firmwareUpdate(formData);
-    } catch (e) {
-      console.log("Erro ao enviar formulario");
-    }
-  });
+    const form = document.getElementById(formId);
+    if (!form) return;
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+        try {
+            Api.fileSystemUpdate(formData);
+        } catch (e) {
+            console.log("Erro ao enviar formulario");
+        }
+    });
 }
